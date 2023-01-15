88b2ad1afba34cbbab307d1703c88fcc5e3436f1
brcaninovict <tarikbrcaninovic245@gmail.com>
2023-01-14 17:53:17 -0800

    forma

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/prijava')
 def login():
-    return render_template('login.html')
+    return render_template('prijava.html')
